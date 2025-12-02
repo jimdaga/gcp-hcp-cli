48a946c97f66ab4395aca7023c6f90a996141e7b
Amador Pahim <apahim@redhat.com>
2025-12-02 09:17:12 +0000

    Revert "GCP-244 | feat: add automatic WIF infrastructure provisioning"

--- src/gcphcp/utils/config.py
+++ src/gcphcp/utils/config.py
@@ -170,11 +170,3 @@ class Config:
         from .. import __version__
 
         return __version__
-
-    def get_hypershift_binary(self) -> Optional[str]:
-        """Get path to hypershift binary.
-
-        Returns:
-            Path to hypershift binary or None if not configured
-        """
-        return self.get("hypershift_binary")
